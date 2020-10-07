7fc208d42fcf0a7a0c43b68291f5bc9f5fdf775f
Camilla Freitas <camirsfreitas@gmail.com>
2020-10-07 17:05:46 -0300

    Verificação de login finalizada.

--- app/src/main/java/br/com/usjt/camilla/cadaluno/ui/LoginActivity.java
+++ app/src/main/java/br/com/usjt/camilla/cadaluno/ui/LoginActivity.java
@@ -1,27 +1,50 @@
 package br.com.usjt.camilla.cadaluno.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.orhanobut.hawk.Hawk;
 
 import br.com.usjt.camilla.cadaluno.R;
+import br.com.usjt.camilla.cadaluno.model.Usuario;
+import br.com.usjt.camilla.cadaluno.model.UsuarioViewModel;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button buttonLogin;
     private TextView textViewNovoCadastro;
+    private UsuarioViewModel usuarioViewModel;
+    private Usuario usuarioCorrente;
+    private EditText editTextEmail;
+    private EditText editTextSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Hawk.init(this).build();
+
+        editTextEmail = findViewById(R.id.editTextUsuario);
+        editTextSenha = findViewById(R.id.editTextSenha);
+
+        usuarioViewModel = new ViewModelProvider(this).get(UsuarioViewModel.class);
+
+        usuarioViewModel.getUsuario().observe(this, new Observer<Usuario>() {
+            @Override
+            public void onChanged(@Nullable final Usuario usuario) {
+                updateUsuario(usuario);
+            }
+        });
 
         buttonLogin = findViewById(R.id.buttonLogin);
         textViewNovoCadastro = findViewById(R.id.textViewNovoCadastro);
@@ -42,6 +65,10 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    private void updateUsuario(Usuario usuario){
+        usuarioCorrente = usuario;
+    }
+
     private void bloquear(){
         buttonLogin.setEnabled(false);
         buttonLogin.setBackgroundColor(getResources().getColor(R.color.cor_cinza));
@@ -60,7 +87,15 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        if(usuarioCorrente != null){
+            if(usuarioCorrente.getEmail().equalsIgnoreCase(editTextEmail.getText().toString())
+            && usuarioCorrente.getSenha().equals(editTextSenha.getText().toString())){
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+            else{
+                Toast.makeText(this, "Usuário e/ou senha inválidos", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
